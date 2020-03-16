6623f44aa37979411f7036b5378e5cb4c85be4ea
rahulsin14 <41705716+rahulsin14@users.noreply.github.com>
2020-03-17 04:12:07 +0530

    added delete button to post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post=require('../models/post');
+const Comment=require('../models/comment');
 module.exports.create=function(req,res){
     Post.create({
         content:req.body.content,
@@ -10,4 +11,16 @@ module.exports.create=function(req,res){
         }
         return res.redirect('back');
     })
+}
+module.exports.destroy=function(req,res){
+    console.log(req.user);
+    Post.findById(req.params.id,function(err,post){
+        if(post.user==req.user.id){
+            post.remove();
+            Comment.deleteMany({post:req.params.id},function(err){
+                return res.redirect('back');
+            });
+            
+        }
+    });
 }
