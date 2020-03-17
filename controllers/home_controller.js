c64150b34bc61aef2708f91f4517069be7ea13ba
rahulsin14 <41705716+rahulsin14@users.noreply.github.com>
2020-03-18 01:07:24 +0530

    added profile links to home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,5 @@
 const Post=require('../models/post');
+const User=require('../models/user');
 module.exports.home = function(req, res){
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
@@ -16,14 +17,15 @@ module.exports.home = function(req, res){
             path:'user'
         }
     })
-    .exec(
-        function(err,posts){
+    .exec(function(err,posts){
+        User.find({},function(err,users){
             return res.render('home', {
                 title: "Home",
-                posts:posts
+                posts:posts,
+                all_users:users
             });
         })
-    
+    })   
 }
 
 // module.exports.actionName = function(req, res){}
