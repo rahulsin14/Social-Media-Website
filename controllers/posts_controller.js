be79a9c2b9cf4991e07722fe9ce60f3efd6e7d6e
rahulsin14 <41705716+rahulsin14@users.noreply.github.com>
2020-03-19 04:08:22 +0530

    added posts and comments using ajax and started using multer

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -24,11 +24,34 @@ module.exports.create=async function(req,res){
     
 };
 module.exports.destroy=async function(req,res){
-    // console.log(req.user);
-    const post =await Post.findById(req.params.id);
-    if(post.user==req.user.id){
-        post.remove();
-        await Comment.deleteMany({post:req.params.id});
-        return res.redirect('back');   
+    try{
+        let post = await Post.findById(req.params.id);
+
+        if (post.user == req.user.id){
+            post.remove();
+
+            await Comment.deleteMany({post: req.params.id});
+
+
+            if (req.xhr){
+                return res.status(200).json({
+                    data: {
+                        post_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
+
+            req.flash('success', 'Post and associated comments deleted!');
+
+            return res.redirect('back');
+        }else{
+            req.flash('error', 'You cannot delete this post!');
+            return res.redirect('back');
+        }
+
+    }catch(err){
+        req.flash('error', err);
+        return res.redirect('back');
     }
 };
