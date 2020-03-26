ad8e93c9fca6f71855beed448ad3b9a90c81045c
rahulsin14 <41705716+rahulsin14@users.noreply.github.com>
2020-03-27 00:01:25 +0530

    added email service

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,36 +1,81 @@
-const Comment=require("../models/comment");
-const Post=require('../models/post');
+const Comment = require('../models/comment');
+const Post = require('../models/post');
+const commentsMailer = require('../mailers/comments_mailer');
+module.exports.create = async function(req, res){
 
-module.exports.create=function(req,res){
-    Post.findById(req.body.post,function(err,post){
-        if(post){
-            Comment.create({
-                content:req.body.content,
-                post:req.body.post,
-                user:req.user._id
-            },function(err,comment){
-                if(err){
-                    console.log('error in adding comment');
-                    return;
-                }
-                post.comments.push(comment);
-                post.save();
-                return res.redirect('/');
+    try{
+        let post = await Post.findById(req.body.post);
+
+        if (post){
+            let comment = await Comment.create({
+                content: req.body.content,
+                post: req.body.post,
+                user: req.user._id
             });
+
+            post.comments.push(comment);
+            post.save();
+            
+            comment = await comment.populate('user', 'name email').execPopulate();
+            commentsMailer.newComment(comment);
+            if (req.xhr){
+                
+    
+                return res.status(200).json({
+                    data: {
+                        comment: comment
+                    },
+                    message: "Post created!"
+                });
+            }
+
+
+            req.flash('success', 'Comment published!');
+
+            res.redirect('/');
         }
-    });
+    }catch(err){
+        req.flash('error', err);
+        return;
+    }
+    
 }
-module.exports.destroy=function(req,res){
-    Comment.findById(req.params.id,function(err,comment){
-        if(comment.user==req.user.id){
-            let postId=comment.post;
+
+
+module.exports.destroy = async function(req, res){
+
+    try{
+        let comment = await Comment.findById(req.params.id);
+
+        if (comment.user == req.user.id){
+
+            let postId = comment.post;
+
             comment.remove();
-            Post.findByIdAndUpdate(postId,{$pull:{comments:req.params.id}},function(err,post){
-                return res.redirect('back');
-            })
+
+            let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+
+            // send the comment id which was deleted back to the views
+            if (req.xhr){
+                return res.status(200).json({
+                    data: {
+                        comment_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
+
+
+            req.flash('success', 'Comment deleted!');
+
+            return res.redirect('back');
         }else{
+            req.flash('error', 'Unauthorized');
             return res.redirect('back');
         }
-       
-    });
+    }catch(err){
+        req.flash('error', err);
+        return;
+    }
+    
 }
